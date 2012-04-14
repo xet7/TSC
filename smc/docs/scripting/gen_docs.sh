81537e42089720586ef3575e997ddb11c06801af
Quintus <sutniuq@gmx.net>
2012-04-14 15:04:31 +0200

    Generate docs into a subdirectory, be more informative and add a nice icon.

--- smc/docs/scripting/gen_docs.sh
+++ smc/docs/scripting/gen_docs.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+echo -n "Creating html/... directories... "
+mkdir -p html/graphics
+echo "Done."
+
+echo -n "Copying graphics... "
+cp "../../data/icon/window_32.png" "html/graphics"
+echo "Done."
+
+echo "Converting Markdown files with kramdown."
 for sourcefile in `ls *.md`; do
     targetfile=`echo $sourcefile | sed s/\.md/\.html/`
-    echo "$sourcefile -> $targetfile"
-    kramdown --toc-levels 2..3 --template template.html.erb $sourcefile > $targetfile
+    echo "$sourcefile -> html/$targetfile"
+    kramdown --toc-levels 2..3 --template template.html.erb $sourcefile > html/$targetfile
 done
+echo "Done."
+
+echo -n "Copying stylesheet... "
+cp style.css html
+echo "Done."
+
+echo "Finished."
+exit 0
