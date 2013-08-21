047a6fdb1e40807046b35bd4b580cf4b8c428d0f
Quintus <quintus@quintilianus.eu>
2013-08-21 16:23:38 +0200

    Fix indentation

--- smc/src/scripting/objects/mrb_eato.cpp
+++ smc/src/scripting/objects/mrb_eato.cpp
@@ -22,10 +22,10 @@ struct mrb_data_type SMC::Scripting::rtEato = {"Eato", NULL};
 static mrb_value Initialize(mrb_state* p_state,  mrb_value self)
 {
 	char* cdir = NULL;
-  mrb_get_args(p_state, "z", &cdir);
-  std::string sdir(cdir);
+	mrb_get_args(p_state, "z", &cdir);
+	std::string sdir(cdir);
 
-  ObjectDirection dir;
+	ObjectDirection dir;
 	if (sdir == "up_left")
 		dir = DIR_UP_LEFT;
 	else if (sdir == "up_right")
@@ -43,24 +43,24 @@ static mrb_value Initialize(mrb_state* p_state,  mrb_value self)
 	else if (sdir == "down_right")
 		dir = DIR_DOWN_RIGHT;
 	else {
-    mrb_raisef(p_state, MRB_ARGUMENT_ERROR(p_state), "Invalid eato direction: %s", cdir);
-    return self; // Not reached
-  }
+		mrb_raisef(p_state, MRB_ARGUMENT_ERROR(p_state), "Invalid eato direction: %s", cdir);
+		return self; // Not reached
+	}
 
-  cEato* p_eato = new cEato(pActive_Level->m_sprite_manager);
-  DATA_PTR(self) = p_eato;
-  DATA_TYPE(self) = &rtEato;
+	cEato* p_eato = new cEato(pActive_Level->m_sprite_manager);
+	DATA_PTR(self) = p_eato;
+	DATA_TYPE(self) = &rtEato;
 
-  // This is a generated object
-  p_eato->Set_Spawned(true);
+	// This is a generated object
+	p_eato->Set_Spawned(true);
 
-  // Required arguments
-  p_eato->Set_Direction(dir);
+	// Required arguments
+	p_eato->Set_Direction(dir);
 
-  // Let SMC manage the memory
-  pActive_Level->m_sprite_manager->Add(p_eato);
+	// Let SMC manage the memory
+	pActive_Level->m_sprite_manager->Add(p_eato);
 
-  return self;
+	return self;
 }
 
 
@@ -74,14 +74,14 @@ static mrb_value Initialize(mrb_state* p_state,  mrb_value self)
 static mrb_value Get_Image_Dir(mrb_state* p_state,  mrb_value self)
 {
 	cEato* p_eato = Get_Data_Ptr<cEato>(p_state, self);
-  return mrb_str_new_cstr(p_state, path_to_utf8(p_eato->m_img_dir).c_str());
+	return mrb_str_new_cstr(p_state, path_to_utf8(p_eato->m_img_dir).c_str());
 }
 
 void SMC::Scripting::Init_Eato(mrb_state* p_state)
 {
-  p_rcEato = mrb_define_class(p_state, "Eato", p_rcEnemy);
-  MRB_SET_INSTANCE_TT(p_rcEato, MRB_TT_DATA);
+	p_rcEato = mrb_define_class(p_state, "Eato", p_rcEnemy);
+	MRB_SET_INSTANCE_TT(p_rcEato, MRB_TT_DATA);
 
-  mrb_define_method(p_state, p_rcEato, "initialize", Initialize, ARGS_REQ(1));
-  mrb_define_method(p_state, p_rcEato, "image_dir", Get_Image_Dir, ARGS_NONE());
+	mrb_define_method(p_state, p_rcEato, "initialize", Initialize, ARGS_REQ(1));
+	mrb_define_method(p_state, p_rcEato, "image_dir", Get_Image_Dir, ARGS_NONE());
 }
