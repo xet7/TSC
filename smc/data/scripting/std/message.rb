e1437a24f4c43824d88391da56c31aaf418edc2f
Quintus <quintus@quintilianus.eu>
2013-10-07 21:01:48 +0200

    Add Std::Message which allows to display textbox-like messages to the user.
    Actually, it uses a TextBox under the hood.

---
+++ smc/data/scripting/std/message.rb
@@ -0,0 +1,40 @@
+module Std
+
+  # Display a message to the user the same way a TextBox
+  # does, i.e. a black box with white text inside appears
+  # and the user has to press the Return key in order to
+  # continue.
+  #
+  # Usage example:
+  #
+  #   m = Message.new("Lorem ipsum dolor sit amet...")
+  #   m.activate
+  #
+  # You can reuse your message as often as you want. Also
+  # note this is a subclass of the core TextBox class,
+  # which especially allows you to change the message text
+  # using the MessageBox#text= method.
+  class Message < TextBox
+
+    # Create a new Message with the given +text+, which
+    # may be multiline.
+    def initialize(text)
+      super
+      self.text = text
+      self.massive_type = :passive
+      hide
+    end
+
+    # Show the message to the user. This method ensures
+    # the text always appears above Maryo.
+    def activate
+      # Ensure we always appear above the player
+      self.start_at(Player.x, Player.y - 100)
+
+      # Actual activation
+      super
+    end
+
+  end
+
+end
