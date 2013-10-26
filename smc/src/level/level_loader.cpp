6fe8fcee2790708f79fbff3c515fb35bf739355e
Quintus <quintus@quintilianus.eu>
2013-10-26 10:43:25 +0200

    Add handling of <property> tags

--- smc/src/level/level_loader.cpp
+++ smc/src/level/level_loader.cpp
@@ -41,12 +41,44 @@ void cLevelLoader::on_end_document()
 
 void cLevelLoader::on_start_element(const Glib::ustring& name, const xmlpp::SaxParser::AttributeList& properties)
 {
+	if (name == "property") {
+		std::string key;
+		std::string value;
 
+		/* Collect all the <property> elements for the surrounding
+		 * mayor element (like <settings> or <sprite>). When the
+		 * surrounding element is closed, the results are handled
+		 * in on_end_element(). */
+		for(xmlpp::SaxParser::AttributeList::const_iterator iter = properties.begin(); iter != properties.end(); iter++) {
+			xmlpp::SaxParser::Attribute attr = *iter;
+
+			if (attr.name == "name")
+				key = attr.value;
+			else if (attr.name == "value")
+				value = attr.value;
+		}
+
+		m_current_properties[key] = value;
+	}
+	else if (name == "script") {
+		// Indicate a script tag has opened, so we can retrieve
+		// its and only its text.
+		m_in_script_tag = true;
+	}
 }
 
 void cLevelLoader::on_end_element(const Glib::ustring& name)
 {
+	// <property> tags are parsed cumulatively in on_start_element()
+	// so all have been collected when the surrounding element
+	// terminates here.
+	if (name == "property")
+		return;
 
+	// Everything handled, so we can now safely clear the
+	// collected <property> element values for the next
+	// tag.
+	m_current_properties.clear();
 }
 
 void cLevelLoader::on_characters(const Glib::ustring& text)
