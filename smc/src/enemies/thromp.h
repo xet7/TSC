f471b40defe33c958daa39526f582d155dc107f8
Quintus <quintus@quintilianus.eu>
2014-03-24 12:36:21 +0100

    Make thromp save with the new XML lib

--- smc/src/enemies/thromp.h
+++ smc/src/enemies/thromp.h
@@ -139,6 +139,11 @@ public:
 	// distance rect to end position
 	GL_rect m_distance_rect;
 
+#ifdef ENABLE_NEW_LOADER
+	// Save to XML node
+	virtual xmlpp::Element* Save_To_XML_Node(xmlpp::Element* p_element);
+#endif
+
 protected:
 	// save to stream
 	virtual void Do_XML_Saving( CEGUI::XMLSerializer &stream );
