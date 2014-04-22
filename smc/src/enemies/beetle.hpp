f457120c4c958c1d80efa1956e61d6e43c1296df
Quintus <quintus@quintilianus.eu>
2014-04-22 20:30:17 +0200

    Add sprites for the beetle.

---
+++ smc/src/enemies/beetle.hpp
@@ -0,0 +1,28 @@
+/***************************************************************************
+ * pip.hpp
+ *
+ * Copyright © 2014 The SMC Contributors
+ ***************************************************************************/
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SMC_BEETLE_HPP
+#define SMC_BEETLE_HPP
+#include "enemy.hpp"
+
+namespace SMC {
+
+	class cBeetle: public cEnemey
+	{
+	};
+
+}
+
+#endif
