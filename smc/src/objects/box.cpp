3b1d4932d619783b0252f0f09bb70fbe2a6354e5
Quintus <quintus@quintilianus.eu>
2014-08-11 20:53:41 +0200

    Fix regression that caused one-time boxes to not fire the activate event.

--- smc/src/objects/box.cpp
+++ smc/src/objects/box.cpp
@@ -458,16 +458,16 @@ void cBaseBox :: Col_Enemy( cSprite *obj )
 
 void cBaseBox :: Activate( void )
 {
-	// set useable count
-	if( m_useable_count > 0 )
-	{
-		Set_Useable_Count( m_useable_count - 1 );
-	}
-
 	if ( m_useable_count > 0 || m_useable_count == -1 )
 	{
 		Scripting::cActivate_Event evt;
 		evt.Fire(pActive_Level->m_mruby, this);
+	}
+
+	// set useable count
+	if( m_useable_count > 0 )
+	{
+		Set_Useable_Count( m_useable_count - 1 );
 	}
 }
 
