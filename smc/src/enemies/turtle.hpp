a85aa30057c40965456b9507c2e3295813e1c9d9
Quintus <quintus@quintilianus.eu>
2014-08-21 19:15:36 +0200

    Resolve Handle_Collision_Box todo.
    Instead of centrally handling how enemies react to a hit from
    a box below it inside cBox, we now delegate that to a call to
    Handle_Collision_Box() so each enemy can decide on its own
    how to act.

--- smc/src/enemies/turtle.hpp
+++ smc/src/enemies/turtle.hpp
@@ -113,6 +113,7 @@ public:
 	virtual void Handle_Collision_Enemy( cObjectCollision *collision );
 	// collision with massive
 	virtual void Handle_Collision_Massive( cObjectCollision *collision );
+	virtual void Handle_Collision_Box( ObjectDirection cdirection, GL_rect *r2 );
 
 	// editor activation
 	virtual void Editor_Activate( void );
