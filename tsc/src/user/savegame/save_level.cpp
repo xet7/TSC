e714ccbbac2b882ca55a8716d1f1af39943afefb
Quintus <quintus@quintilianus.eu>
2015-02-26 22:26:46 +0100

    Separate cSave_Level class

---
+++ tsc/src/user/savegame/save_level.cpp
@@ -0,0 +1,93 @@
+/***************************************************************************
+ * save_level.cpp - Handler for saving a cLevel instance to a savegame.
+ *
+ * Copyright © 2003 - 2011 Florian Richter
+ * Copyright © 2013 - 2014 The TSC Contributors
+ ***************************************************************************/
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "save_level.hpp"
+
+/* *** *** *** *** *** *** *** cSave_Level *** *** *** *** *** *** *** *** *** *** */
+
+using namespace TSC;
+
+cSave_Level::cSave_Level(void)
+{
+    // level
+    m_active = false;
+    m_level_pos_x = 0.0f;
+    m_level_pos_y = 0.0f;
+}
+
+cSave_Level::~cSave_Level(void)
+{
+    for (cSprite_List::iterator itr = m_regular_objects.begin(); itr != m_regular_objects.end(); ++itr) {
+        delete *itr;
+    }
+
+    m_regular_objects.clear();
+
+    for (cSprite_List::iterator itr = m_spawned_objects.begin(); itr != m_spawned_objects.end(); ++itr) {
+        delete *itr;
+    }
+
+    m_spawned_objects.clear();
+}
+
+cSave_Level::Save_To_Node(xmlpp::Element* p_parent_node)
+{
+    cSprite_List::const_iterator iter;
+
+    // <level>
+    xmlpp::Element* p_node = p_parent_node->add_child("level");
+    Add_Property(p_node, "level_name", m_name);
+
+    // Player position. Only save that for the active level.
+    if (m_active) {
+        Add_Property(p_node, "player_posx", p_level->m_level_pos_x);
+        Add_Property(p_node, "player_posy", p_level->m_level_pos_y);
+    }
+
+    /* Custom data a script writer wants to store; empty if the
+     * script writer didn’t hook into the on_load and on_save
+     * events. */
+    if (!m_mruby_data.empty())
+        Add_Property(p_node, "mruby_data", p_level->m_mruby_data);
+
+    // The regular objects.
+    // <object_data>
+    xmlpp::Element* p_objects_data_node->add_child("objects_data");
+    for(iter=m_regular_objects.begin(); iter != m_regular_objects.end(); iter++) {
+        cSprite* p_sprite = (*iter);
+
+        /* TODO: Have the sprite itself decide whether it wants to be
+         * saved or not by not adding anything to the node and
+         * returning false from Save_To_Savegame_XML_Node(). For now,
+         * just assume TYPE_UNDEFINED sprites, i.e. all static
+         * nonmoving sprites, do not need to be saved. */
+        if (p_sprite->m_type != TYPE_UNDEFINED) {
+            xmlpp::Element* p_object_node = p_objects_data_node->add_child("object");
+            p_sprite->Save_To_Savegame_XML_Node(p_object_node);
+        }
+    }
+
+    // The spawned objects. These have always to be saved.
+    // <spawned_objects>
+    xmlpp::Element* p_spawned_node = p_node->add_child("spawned_objects");
+    for(iter=m_spawned_objects.begin(); iter != m_spawned_objects.end(); iter++) {
+        cSprite* p_sprite = (*iter);
+        p_sprite->Save_To_XML_Node(p_spawned_node);
+    }
+    // </spawned_objects>
+
+    //</level>
+}
--- tsc/src/user/savegame/savegame.hpp
+++ tsc/src/user/savegame/savegame.hpp
@@ -57,37 +57,6 @@ namespace TSC {
 
     typedef vector<cSave_Overworld*> Save_OverworldList;
 
-    /* *** *** *** *** *** *** *** cSave_Level *** *** *** *** *** *** *** *** *** *** */
-    /**
-     * Represents a cLevel instance in the savegame containing a list of
-     * all spawned sprites. Note a single savegame may include multiple
-     * levels when a user saves while or after he visited a sublevel
-     */
-    class cSave_Level {
-    public:
-        cSave_Level(void);
-        ~cSave_Level(void);
-
-        void Save_To_Node(xmlpp::Element* p_parent_node);
-
-        std::string m_name;
-        /// True if this is the active level.
-        bool is_active;
-        // player position is only set if level is the active one
-        float m_level_pos_x;
-        float m_level_pos_y;
-
-        /// List of objects that originate from the level XML.
-        cSprite_List m_regular_objects;
-        /// List of spawned objects (i.e. not from the level XML).
-        cSprite_List m_spawned_objects;
-
-        // Data a script writer wants to store
-        std::string m_mruby_data;
-    };
-
-    typedef vector<cSave_Level*> Save_LevelList;
-
     /* *** *** *** *** *** cSave_Player_Return_Entry *** *** *** *** *** *** *** *** */
     class cSave_Player_Return_Entry {
     public:
