3758e7e1e9f7fb32f8ab590201c8d1146e2400ca
Quintus <quintus@quintilianus.eu>
2015-04-02 15:17:15 +0200

    cActor as sf::Transformable.

--- tsc/src/objects/actor.cpp
+++ tsc/src/objects/actor.cpp
@@ -7,31 +7,76 @@ using namespace TSC;
 cActor::cActor()
     : cScriptable_Object()
 {
-    //
+    // Some sensible defaults for a collision rectangle so it’s not invisible
+    // on debugging if unset.
+    m_collision_rect.left = 0;
+    m_collision_rect.top = 0;
+    m_collision_rect.width = 100;
+    m_collision_rect.height = 100;
+
+    m_name = "(Unnamed actor)";
 }
 
 cActor::~cActor()
 {
 }
 
+/**
+ * Update this actor for the upcoming frame drawing.
+ */
 void cActor::Update()
 {
-}
-
-void cActor::Draw(sf::RenderWindow& stage)
-{
-    stage.draw(m_sprite);
+    // Virtual
 }
 
 /**
- * Specify the visual dimensions of the actor. This also resets
- * the collision rectangle to exactly the same dimension; use
- * Set_Collision_Rect() to specify another collision rectangle.
+ * Draw this actor onto the given window.
  */
-void cActor::Set_Dimensions(int width, int height)
+void cActor::Draw(RenderTarget& stage)
 {
-    sf::FloatRect normal_rect = m_sprite.getLocalBounds();
-    m_sprite.setScale(width / normal_rect.width, height / normal_rect.height);
+    // Virtual
+}
 
-    m_collision_rect = sf::FloatRect(0, 0, width, height);
+
+/**
+ * Retrieves the collision rect how it looks after being transformed.
+ * Note SFML only supports 90° step rotations on rectangles so until
+ * someone implements this in our code, no finer rotation resolution
+ * is possible. If you try, you’ll get the bounding rectangle of the
+ * transformed rectangle back. Other transformations should work fine.
+ *
+ * \see [sf::Transform::transformRect()](http://www.sfml-dev.org/documentation/2.2/classsf_1_1Transform.php#a345112559981d988e92b54b7976fca8a)
+ */
+sf::FloatRect cActor::Get_Transformed_Collision_Rect() const
+{
+    return getTransform().transformRect(m_collision_rect);
+}
+
+/**
+ * Checks if this actor’s collision rectangle collides with
+ * the given other rectangle. Rectangles collide if they intersect.
+ */
+bool cActor::Does_Collide(const sf::FloatRect& other_rect) const
+{
+    return Get_Transformed_CollisionRect().intersects(other_rect);
+}
+
+/**
+ * Checks if this actor’s collision rectangle collides with
+ * the given other point. A point collides with a rectangle if
+ * it is contained in it.
+ */
+bool cActor::Does_Collide(const sf::Vector2f& other_point) const
+{
+    return Get_Transformed_Collision_Rect().contains(other_point);
+}
+
+/**
+ * Checks if this actor’s collision rectangle collides with
+ * the given other actor’s collision rectangle. Two rectangles
+ * collide if they intersect.
+ */
+bool cActor::Does_Collide(const cActor& other_actor) const
+{
+    return Get_Transformed_Collision_Rect().intersects(other_actor.Get_Transformed_Collision_Rect());
 }
--- tsc/src/objects/actor.hpp
+++ tsc/src/objects/actor.hpp
@@ -8,27 +8,30 @@ namespace TSC {
      * elements such as the HUD.
      *
      * This is an abstract class that is intended to be subclassed.
+     *
+     * Note this class inherits from [sf::Transformable](http://www.sfml-dev.org/documentation/2.2/classsf_1_1Transformable.php).
      */
-    class cActor: public Scripting::cScriptable_Object
+    class cActor: public Scripting::cScriptable_Object, sf::Transformable
     {
     public:
         cActor();
         virtual ~cActor();
 
         virtual void Update();
-        virtual void Draw(sf::RenderWindow& stage);
-
-        void Set_Dimensions(int width, int height);
+        virtual void Draw(sf::RenderWindow& stage) const;
 
         inline void Set_Collision_Rect(sf::FloatRect rect){m_collision_rect = rect;}
-        inline const sf::FloatRect& Get_Collision_Rect(){return m_collision_rect;}
+        inline const sf::FloatRect& Get_Collision_Rect() const {return m_collision_rect;}
+
+        sf::FloatRect Get_Transformed_Collision_Rect() const;
+        bool Does_Collide(const sf::FloatRect& other_rect) const;
+        bool Does_Collide(const sf::Vector2f& other_point) const;
+        bool Does_Collide(const cActor& other_actor) const;
 
         inline void Set_Name(std::string name){m_name = name;}
-        inline std::string Get_Name(){return m_name;}
-
-        inline sf::Sprite& Get_Sprite() {return m_sprite;}
+        inline std::string Get_Name() const {return m_name;}
     protected:
-        sf::Sprite m_sprite;
+
         sf::FloatRect m_collision_rect;
         std::string m_name;
     };
