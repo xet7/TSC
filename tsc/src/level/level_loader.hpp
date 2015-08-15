2350f33cf08c34940108bcc3bec69e82cdb61cf9
Quintus <quintus@quintilianus.eu>
2015-08-15 11:13:07 +0200

    Prepare level loader for loading boxes.

--- tsc/src/level/level_loader.hpp
+++ tsc/src/level/level_loader.hpp
@@ -69,7 +69,7 @@ namespace TSC {
         static std::vector<cActor*> Create_Enemy_Stoppers_From_XML_Tag(const std::string& name, XmlAttributes& attributes, cLevel& level, int engine_version);
         // OLD static std::vector<cActor*> Create_Level_Exits_From_XML_Tag(const std::string& name, XmlAttributes& attributes, int engine_version, cSprite_Manager* p_sprite_manager);
         // OLD static std::vector<cActor*> Create_Level_Entries_From_XML_Tag(const std::string& name, XmlAttributes& attributes, int engine_version, cSprite_Manager* p_sprite_manager);
-        // OLD static std::vector<cActor*> Create_Boxes_From_XML_Tag(const std::string& name, XmlAttributes& attributes, int engine_version, cSprite_Manager* p_sprite_manager);
+        static std::vector<cActor*> Create_Boxes_From_XML_Tag(const std::string& name, XmlAttributes& attributes, cLevel& level, int engine_version);
         // OLD static std::vector<cActor*> Create_Items_From_XML_Tag(const std::string& name, XmlAttributes& attributes, int engine_version, cSprite_Manager* p_sprite_manager);
         // OLD static std::vector<cActor*> Create_Moving_Platforms_From_XML_Tag(const std::string& name, XmlAttributes& attributes, int engine_version, cSprite_Manager* p_sprite_manager);
         // OLD static std::vector<cActor*> Create_Falling_Platforms_From_XML_Tag(const std::string& name, XmlAttributes& attributes, int engine_version, cSprite_Manager* p_sprite_manager);
