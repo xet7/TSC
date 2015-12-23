37c6fd8a344ab29f11d8622de8fff57c0943fa96
Quintus <quintus@quintilianus.eu>
2015-12-23 18:09:36 +0100

    Port joystick to SFML. Closes #491.
    There might be some old joystick references left in the code elsewhere
    than joystick.cpp. I can’t guarantee there aren’t any, because due to
    the other pending SFML port issues the codebase as a whole does not yet
    successfully compile.

--- tsc/src/input/joystick.hpp
+++ tsc/src/input/joystick.hpp
@@ -41,22 +41,17 @@ namespace TSC {
         // Resets all Buttons and modifiers
         void Reset_keys(void);
 
-        // Handle the Hat
-        void Handle_Hat(SDL_Event* ev);
         // Handles the Joystick motion
-        void Handle_Motion(SDL_Event* ev);
+        void Handle_Motion(const sf::Event& evt);
         // Handle Joystick Button down event
-        bool Handle_Button_Down_Event(SDL_Event* ev);
+        bool Handle_Button_Down_Event(const sf::Event& evt);
         // Handle Joystick Button up event
-        bool Handle_Button_Up_Event(SDL_Event* ev);
+        bool Handle_Button_Up_Event(const sf::Event& evt);
 
         // Returns the current Joystick name
         std::string Get_Name(void) const;
         // Returns all available Joystick names
         vector<std::string> Get_Names(void) const;
-
-        // Sets the given button state
-        void Set_Button(Uint8 button, bool pressed);
 
         // check if the analog direction is pressed
         bool Left(void) const;
@@ -64,7 +59,7 @@ namespace TSC {
         bool Up(void) const;
         bool Down(void) const;
         // check if the given button is pushed
-        bool Button(Uint8 button);
+        bool Button(unsigned int button);
 
         // analog directions
         bool m_left;
