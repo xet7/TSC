2a36c65378978211998c0ff235a3be8d4fcbe828
Quintus <quintus@quintilianus.eu>
2016-01-01 22:09:41 +0100

    Port level loader to new font manager.
    Actually, don’t show file names on unavailable images anymore. Since
    texts are no more sprites, this would become too compilcated. A
    placeholder image is added instead.

--- tsc/src/level/level_loader.cpp
+++ tsc/src/level/level_loader.cpp
@@ -475,17 +475,11 @@ std::vector<cSprite*> cLevelLoader::Create_Sprites_From_XML_Tag(const std::strin
 
     cSprite* p_sprite = new cSprite(attributes, p_sprite_manager);
 
-    // If image not available display its filename
+    // If image not available display placeholder
     if (!p_sprite->m_start_image) {
-        std::string text = attributes["image"];
-        if (text.empty())
-            text = "Invalid image here";
-
-        cGL_Surface* p_text_image = pFont->Render_Text(pFont->m_font_small, text);
-        p_text_image->m_path = utf8_to_path(text);
-        p_sprite->Set_Image(p_text_image, true, true);
-        p_sprite->Set_Massive_Type(MASS_PASSIVE); // It shouldn't hinder gameplay
-        p_sprite->Set_Active(false); // Only display it in the editor
+        p_sprite->Set_Image(pVideo->Get_Package_Surface(utf8_to_path("game/image_not_found.png")), true, true);
+        p_sprite->Set_Massive_Type(MASS_PASSIVE); // It sholdn't hinder gameplay
+        p_sprite->Set_Active(false); // only display it in the editor
     }
 
     // needs image
