0476916d497c8ea53d23c002aca64f4546c0d590
Taran Lynn <taranlynn0@gmail.com>
2016-01-02 09:29:02 -0800

    Replaced old SDL_GL_SwapBuffers with SFML equivalent.

--- tsc/src/gui/menu_data.cpp
+++ tsc/src/gui/menu_data.cpp
@@ -2533,6 +2533,7 @@ bool cMenu_Options::Video_Button_Apply_Clicked(const CEGUI::EventArgs& event)
 
     pGuiSystem->renderGUI();
     pRenderer->Render();
+    pVideo->mp_window->display();
 
     // apply new settings
     pPreferences->Apply_Video(m_vid_w, m_vid_h, m_vid_bpp, m_vid_fullscreen, m_vid_vsync, m_vid_geometry_detail, m_vid_texture_detail);
