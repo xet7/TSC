e2a9ada2c7aceeb5847722730f2ed9d2c0d4425b
Quintus <quintus@quintilianus.eu>
2016-01-05 13:45:59 +0100

    Fix endless recursion in debug HUD.

--- tsc/src/gui/hud.cpp
+++ tsc/src/gui/hud.cpp
@@ -697,7 +697,7 @@ void cFpsDisplay::Draw()
         return;
     }
 
-    cFpsDisplay::Draw();
+    cStatusText::Draw();
 }
 
 /* *** *** *** *** *** cInfoMessage *** *** *** *** *** *** *** *** *** *** *** */
